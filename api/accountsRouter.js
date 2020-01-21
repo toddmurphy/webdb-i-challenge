410eb25e00fc686061966db5073f07cb028c0f8b
toddmurphy <toddmurphy93@gmail.com>
2020-01-20 23:36:42 -0400

    get all accounts working

--- api/accountsRouter.js
+++ api/accountsRouter.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const db = require('../data/dbConfig');
 const router = express.Router();
 
 //get accounts --> all accounts
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   try {
+    const accounts = await db('accounts').select();
+    res.json(accounts);
   } catch (error) {
     res.status(500).json({ message: 'Sorry, no accounts returned from server', error });
   }
 });
 
 //get account --> single account by id
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
   } catch (error) {
     res.status(500).json({ message: 'Sorry, no single account returned from server', error });
@@ -18,7 +21,7 @@ router.get('/:id', (req, res) => {
 });
 
 //post --> create new account
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   try {
   } catch (error) {
     res.status(500).json({ message: 'Sorry, no account created on the server', error });
@@ -26,7 +29,7 @@ router.post('/', (req, res) => {
 });
 
 //delete --> delete account by id
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   try {
   } catch (error) {
     res.status(500).json({ message: 'Sorry, no accounted deleted from the server', error });
@@ -34,7 +37,7 @@ router.delete('/:id', (req, res) => {
 });
 
 //put --> updated account by id
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
   } catch (error) {
     res.status(500).json({ message: 'Sorry, account not updated on the server', error });
